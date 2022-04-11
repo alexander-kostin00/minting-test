b05b8be6bd4e97e3a6327ca7c347a3729a58d527
SASHA PETRENKO <alex_kostin2000@mail.ru>
2022-04-11 04:38:46 +0200

    ffff

--- backend/src/config.js
+++ backend/src/config.js
@@ -70,7 +70,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = ""; // If you want to manually include it
+let CONTRACT_ADDRESS = "0xcedE0ba29b1DD6771D1B9e65422463f748F8414C"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
