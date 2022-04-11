c1185edbe235d5ac72316bdcb81de5a69c613a02
SASHA PETRENKO <alex_kostin2000@mail.ru>
2022-04-11 04:23:15 +0200

    max per wallet changed

--- backend/src/config.js
+++ backend/src/config.js
@@ -56,7 +56,7 @@ const OWNER_ADDRESS = '0xF7b6c9e3292030398825efB259abe77694E08135';
 const TREASURY_ADDRESS = '0xF7b6c9e3292030398825efB259abe77694E08135';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 0.15; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 1000; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-04-08T00:30:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
